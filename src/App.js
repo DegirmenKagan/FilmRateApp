89c35d4ae03d95519ecf8e92dfebe5b2e0ab8301
Kağan Değirmen <kagandegirmen@gmail.com>
2023-07-17 00:38:51 +0300

    app js draft, before components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 const App = () => {
-  return <h1>App</h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+    const data = await response.json();
+  };
+
+  useEffect(() => {
+    // searchMovies("baby driver");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>FilmRateApp</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={"baby driver"}
+          onClick={() => {}}
+        />
+        <img src={SearchIcon} alt="searchicon" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{item.Year}</p>
+          </div>
+          <div>
+            {" "}
+            <img
+              src={item.Poster ?? "https://via.placeholder.com/400"}
+              alt={item.Title}
+            />
+          </div>
+          <div>
+            <span>{item.Type}</span>
+            <h3>{item.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
